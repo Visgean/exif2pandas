267022f8754d4d8fe281423f6f8f9791c5ca19b2
Visgean Skeloru <me@visgean.me>
2019-06-30 16:10:56 +0200

    add processes argument

--- exif2pandas/main.py
+++ exif2pandas/main.py
@@ -12,11 +12,22 @@ parser.add_argument(
     help='location for the sqlite db file',
     default='photos.sqlite'
 )
+parser.add_argument(
+    '-p', '--processes',
+    type=int,
+    help='number of processes to use for collecting exif data, defaults to 5',
+    default=5
+)
+
 
 def main():
     args = parser.parse_args()
 
-    df = utils.get_panda_dataframe([Path(f).resolve() for f in args.picture_folders])
+    df = utils.get_panda_df(
+        [Path(f).resolve() for f in args.picture_folders],
+        processes=args.processes
+    )
+    print(df)
 
 
 
